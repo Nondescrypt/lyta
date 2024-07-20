c0768c477afbe681f6cf87a3db946c22901020ed
Nondescrypt <nondescriptenterprises@gmail.com>
2024-07-20 15:58:54 -0700

    use @testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,11 @@ using Test
 println("Running tests for package Lyta...")
 
 p = Lyta.point(1,2,3);
-v = Lyta.vector(-1,-2,-3);
+v = Lyta.vector(1,2,3);
 
 # silly tests
-@test p.t == Lyta.t_point
-@test v.t == Lyta.t_vector;
-
+@testset "constructors" begin
+    @test p.t == Lyta.t_point;
+    @test v.t == Lyta.t_vector;
+    @test p != v;
+end
