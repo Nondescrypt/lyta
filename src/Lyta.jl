0d93ed073cedc55b032c4d982f0fef408d0c478f
Nondescrypt <nondescriptenterprises@gmail.com>
2024-07-20 15:49:22 -0700

    define points and vectors; add silly tests

--- src/Lyta.jl
+++ src/Lyta.jl
@@ -1,5 +1,22 @@
 module Lyta
 
-greet() = print("Lyta en la casa!")
+    greet() = print("Lyta en la casa!")
+
+    @enum TripleType t_vector=0 t_point=1
+
+    struct Triple
+        x::Float64
+        y::Float64
+        z::Float64
+        t::TripleType
+    end;
+
+    function vector(x, y, z)
+        Triple(x, y, z, t_vector);
+    end
+
+    function point(x, y, z)
+        Triple(x, y, z, t_point);
+    end
 
 end # module Lyta
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,12 @@
+using Lyta
+using Test
+
 println("Running tests for package Lyta...")
+
+p = Lyta.point(1,2,3);
+v = Lyta.vector(-1,-2,-3);
+
+# silly tests
+@test p.t == Lyta.t_point
+@test v.t == Lyta.t_vector;
+
