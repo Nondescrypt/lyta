f6b551f7b2c40d749708cadc88a07b83493203ee
Nondescrypt <nondescriptenterprises@gmail.com>
2024-07-20 16:56:22 -0700

    arithmetic operations

--- src/Lyta.jl
+++ src/Lyta.jl
@@ -1,10 +1,21 @@
 module Lyta
 
+import Base.+;
+import Base.-;
+import Base.*;
+
     greet() = print("Lyta en la casa!")
 
     @enum TripleType t_vector=0 t_point=1
 
-    struct Triple
+    struct Vector
+        x::Float64
+        y::Float64
+        z::Float64
+        t::TripleType
+    end;
+
+    struct Point
         x::Float64
         y::Float64
         z::Float64
@@ -12,11 +23,20 @@ module Lyta
     end;
 
     function vector(x, y, z)
-        Triple(x, y, z, t_vector);
+        Vector(x, y, z, t_vector);
     end
 
     function point(x, y, z)
-        Triple(x, y, z, t_point);
+        Point(x, y, z, t_point);
     end
+
+    (+)(a::Vector, b::Vector) = vector(a.x + b.x, a.y + b.y, a.z + b.z);
+    (+)(a::Point, b::Vector) = point(a.x + b.x, a.y + b.y, a.z + b.z);
+
+    (-)(a::Vector, b::Vector) = vector(a.x - b.x, a.y - b.y, a.z - b.z);
+    (-)(a::Point, b::Vector) = point(a.x - b.x, a.y - b.y, a.z - b.z);
+    (-)(a::Vector) = vector(-a.x, -a.y, -a.z);                              # unary negation
+
+    (*)(a::Number, b::Vector) = vector(a * b.x, a * b.y, a * b.z );
 
 end # module Lyta
--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,13 @@ v = Lyta.vector(1,2,3);
     @test v.t == Lyta.t_vector;
     @test p != v;
 end
+
+@testset "arithmetic" begin
+    v1 = Lyta.vector(-1, -2, -3);
+    @test p + v1 == Lyta.point(0,0,0);      # point plus vector is a point
+    @test v + v1 == Lyta.vector(0,0,0);     # vector plus vector is a vector
+    @test p - v == Lyta.point(0,0,0);       
+    @test v - v1 == Lyta.vector(2,4,6);
+    @test -v + v == Lyta.vector(0,0,0);     # unary negation
+    @test -2 * v1 == Lyta.vector(2, 4, 6);  # multiplication by a scalar
+end
